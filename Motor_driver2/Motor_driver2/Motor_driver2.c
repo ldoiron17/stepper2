1c18008da925ce04b32bf1d0c7544b14e2e3a9fe
ldoiron17 <ldoiron17@gmail.com>
2013-10-09 15:46:08 -0700

    Checking output pins, they  work correctly.

--- Motor_driver2/Motor_driver2/Motor_driver2.c
+++ Motor_driver2/Motor_driver2/Motor_driver2.c
@@ -5,13 +5,25 @@
  *  Author: Cornelius Fudge
  */ 
 
+# define F_CPU 1000000UL
 
 #include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
 
 int main(void)
 {
+	DDRC  = 0b11111111;
     while(1)
     {
-        //TODO:: Please write your application code 
+        PORTC |= _BV(0);
+		PORTC |= _BV(1);
+		PORTC |= _BV(2);
+		PORTC |= _BV(3);
+		PORTC |= _BV(4);
+		PORTC |= _BV(5);
+		PORTC |= _BV(6);
+		PORTC |= _BV(7);
     }
 }
