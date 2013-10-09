0ffee0cd29de90945946636e4ae309b1676a65b7
ldoiron17 <ldoiron17@gmail.com>
2013-10-09 10:30:54 -0700

    initial commit

---
+++ Motor_driver2/Motor_driver2/Motor_driver2.c
@@ -0,0 +1,17 @@
+/*
+ * Motor_driver2.c
+ *
+ * Created: 10/9/2013 1:29:00 PM
+ *  Author: Cornelius Fudge
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
