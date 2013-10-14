664f3d489aa5ab41bee2455e47f17fa07b58e44d
ldoiron17 <ldoiron17@gmail.com>
2013-10-14 14:20:03 -0700

    Got motors spinning! Created two 90 degrees out of phase square waves for the two phases of the stepper to run! Need to add ADC code to read the current sense resistor voltage and create a current hysterisis Also need to write USB to UART code to communicate with the PC

--- Motor_driver2/Motor_driver2/Motor_driver2.c
+++ Motor_driver2/Motor_driver2/Motor_driver2.c
@@ -17,43 +17,69 @@
 #define PWM3A_off()  PORTG &= ~_BV(2)
 #define PWM3B_on()  PORTA |= _BV(7)
 #define PWM3B_off()  PORTA &= ~_BV(7)
-#define DIS3_on()  PORTA |= _BV(6)     //Disable Motor Driver Chip 3
-#define DIS3_off()  PORTA &= ~_BV(6)   //Enable Motor Driver Chip 3
+#define DIS3_on()  PORTA |= _BV(6)     //Disable Motor Driver Chip 2
+#define DIS3_off()  PORTA &= ~_BV(6)   //Enable Motor Driver Chip 2
+
+
+/*
+#define PWM2A_on()  PORTA |= _BV(5)
+#define PWM2A_off()  PORTA &= ~_BV(5)
+#define PWM2B_on()  PORTA |= _BV(4)
+#define PWM2B_off()  PORTA &= ~_BV(4)
+#define DIS2_on()  PORTA |= _BV(3)     //Disable Motor Driver Chip 1
+#define DIS2_off()  PORTA &= ~_BV(3)   //Enable Motor Driver Chip 1
+*/
 
 #define PWM2A_on()  PORTA |= _BV(5)
 #define PWM2A_off()  PORTA &= ~_BV(5)
 #define PWM2B_on()  PORTA |= _BV(4)
 #define PWM2B_off()  PORTA &= ~_BV(4)
-#define DIS2_on()  PORTA |= _BV(3)     //Disable Motor Driver Chip 2
-#define DIS2_off()  PORTA &= ~_BV(3)   //Enable Motor Driver Chip 2
+#define DIS2_on()  PORTA |= _BV(3)     //Disable Motor Driver Chip 3
+#define DIS2_off()  PORTA &= ~_BV(3)   //Enable Motor Driver Chip 3
 
-/*
-ISR (TIMER1_OVF_vect)
-{
-	PWM1A_on();
-};
+int volatile count = 0;
 
-ISR (TIMER1_COMPB_vect)
-{
-	PWM1A_off();
-};
-*/
 	ISR(TIMER1_COMPA_vect) //interrupt service routine for timer1 compare A flag
 	{
-		PWM2A_on();
-		//PWM3A_on();
-		//PWM3B_off();
-		PWM2B_off();
+		//PWM2A_on();
+		//PWM2B_off();
 	}
 
 	ISR(TIMER1_COMPB_vect) //interrupt service routine for timer1 compare B flag
 	{
-		PWM2A_off();
-		//PWM3A_off();
-		//PWM3B_on();
-		PWM2B_on();
+		//PWM2A_off();
+		//PWM2B_on();
+	}
+	
+	ISR(TIMER3_COMPA_vect) //interrupt service routine for timer1 compare A flag
+	{
+		if(count == 0){
+			count += 1;
+			PWM3A_on();
+			PWM3B_off();
+		}else if(count == 1){
+			count += 1;
+			PWM2A_on();
+			PWM2B_off();
+		}else if(count == 2){
+		count += 1;
+		PWM3A_off();
+		PWM3B_on();
+		} else{
+			PWM2A_off();
+			PWM2B_on();
+			count = 0;
+		}
 	}
 
+	ISR(TIMER3_COMPB_vect) //interrupt service routine for timer1 compare B flag
+	{
+		//PWM3A_off();
+		//PWM3B_on();
+	}
+	
+	
+	
 int main(void)
 {
 	//Set direction registers (1 -> output, 0 -> input)
@@ -62,28 +88,24 @@ int main(void)
 	DDRB  = 0b11110000; //set OC1A and OC1B to outputs for use with PWM
 	PORTB = 0x00;
 
+	//Set up PWM timers
 	TIMSK = (1 << TOIE1) | (1 << OCIE1A) | (1 << OCIE1B);
+	ETIMSK = (1 << TOIE3) | (1 << OCIE3A) | (1 << OCIE3B);
 	
-	TCCR1A = (0 << WGM01) | (1 << WGM00);
+	TCCR3A = (0 << WGM31) | (0 << WGM30);// | (1 << COM3A0) | (1 << COM3B0);
+	TCCR3B = (0 << WGM33) | (1 << WGM32)| (1 << CS32) |(0 << CS31) | (1 << CS30);
+	TCCR1A = (0 << WGM01) | (0 << WGM00);// | (1 << COM1A0) | (1 << COM1B0);
 	TCCR1B = (0 << WGM13) | (1 << WGM12) | (1 << CS02) |(0 << CS01) | (1 << CS00);
 	//Note OCR1B must be LESS THAN OCR1A
-	OCR1A = 2000;  //set PWM frequency  100*(1/8e6) =  12.5e-6 seconds                  /
-	OCR1B = 1000;   //set Duty Ratio     100*(1/8e6) =  12.5e-6 seconds              
+	OCR1A = 2000;  //set PWM frequency  1000*(1/8e6) =  12.5e-5 seconds  (4KHz)               
+	OCR1B = 1000;   //set Duty Ratio     1000*(1/8e6) =  12.5e-5 seconds (4KHz)    
+	OCR3A = 100;  //set PWM frequency  100*(1/8e6) =  12.5e-5 seconds               
+	OCR3B = 50;   //set Duty Ratio     100*(1/8e6) =  12.5e-5 seconds              
 	sei();
 	
-	//Set up PWM timers
-	
-	//TIMSK = _BV(OCIE1A) | _BV(OCIE1B); // Enable Interrupt Timer/Counter1, Output Compare A & B (SIG_OUTPUT_COMPARE1A/SIG_OUTPUT_COMPARE1B)
-	//TCCR1A = _BV(COM1A1) | _BV(COM1B1);
-	//TCCR1B = _BV(CS12) | _BV(CS10) | _BV(WGM12);    // Clock/1024, 0.001024 seconds per tick, Mode=CTC
 
-	
-/*	
-	TCCR2 |= _BV(WGM21); //set to CTC mode
-	TCCR2 |= _BV(CS22);
-	TCCR2 |= _BV(CS20);
-	TIMSK |= _BV(OCIE2); //turn output of timer on
-*/
+	count = 0;
+
 
 	DIS3_off();
 	PWM3A_off();
@@ -97,8 +119,7 @@ int main(void)
 	_delay_ms(1000); //delay 1 second before turning on gate driver	
 	DIS2_off();
 	DIS3_off();
-	PWM3A_on();
-	PWM3B_on();
+
 	
 	while(1){
 		
