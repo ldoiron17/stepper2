2ec576b4c380e0c798c41e7847b21b4004169b14
ldoiron17 <ldoiron17@gmail.com>
2013-10-15 22:32:50 -0700

    started writing ADC code and commenting other parts of previous code

--- Motor_driver2/Motor_driver2/Motor_driver2.c
+++ Motor_driver2/Motor_driver2/Motor_driver2.c
@@ -21,15 +21,6 @@
 #define DIS3_off()  PORTA &= ~_BV(6)   //Enable Motor Driver Chip 2
 
 
-/*
-#define PWM2A_on()  PORTA |= _BV(5)
-#define PWM2A_off()  PORTA &= ~_BV(5)
-#define PWM2B_on()  PORTA |= _BV(4)
-#define PWM2B_off()  PORTA &= ~_BV(4)
-#define DIS2_on()  PORTA |= _BV(3)     //Disable Motor Driver Chip 1
-#define DIS2_off()  PORTA &= ~_BV(3)   //Enable Motor Driver Chip 1
-*/
-
 #define PWM2A_on()  PORTA |= _BV(5)
 #define PWM2A_off()  PORTA &= ~_BV(5)
 #define PWM2B_on()  PORTA |= _BV(4)
@@ -51,7 +42,7 @@ int volatile count = 0;
 		//PWM2B_on();
 	}
 	
-	ISR(TIMER3_COMPA_vect) //interrupt service routine for timer1 compare A flag
+	ISR(TIMER3_COMPA_vect) //interrupt service routine for timer3 compare A flag
 	{
 		if(count == 0){
 			count += 1;
@@ -82,16 +73,19 @@ int volatile count = 0;
 	
 int main(void)
 {
-	//Set direction registers (1 -> output, 0 -> input)
+	//Set direction registers for GPIO (1 -> output, 0 -> input)
 	DDRA  = 0b11111111;
 	DDRG  = 0b00000100;
 	DDRB  = 0b11110000; //set OC1A and OC1B to outputs for use with PWM
-	PORTB = 0x00;
+	PORTB = 0x00; //initialize PORTB to zero
 
-	//Set up PWM timers
-	TIMSK = (1 << TOIE1) | (1 << OCIE1A) | (1 << OCIE1B);
-	ETIMSK = (1 << TOIE3) | (1 << OCIE3A) | (1 << OCIE3B);
+/************************Set up PWM timers***************************************/
+	TIMSK = //Timer/Counter Interrupt Mask Register
+		(1 << TOIE1) |  //Overflow Interrupt Enable
+		(1 << OCIE1A) | //Output Compare A Match Interrupt Enable
+		(1 << OCIE1B);  //Output Compare B Match Interrupt Enable
 	
+	ETIMSK = (1 << TOIE3) | (1 << OCIE3A) | (1 << OCIE3B);	
 	TCCR3A = (0 << WGM31) | (0 << WGM30);// | (1 << COM3A0) | (1 << COM3B0);
 	TCCR3B = (0 << WGM33) | (1 << WGM32)| (1 << CS32) |(0 << CS31) | (1 << CS30);
 	TCCR1A = (0 << WGM01) | (0 << WGM00);// | (1 << COM1A0) | (1 << COM1B0);
@@ -99,9 +93,29 @@ int main(void)
 	//Note OCR1B must be LESS THAN OCR1A
 	OCR1A = 2000;  //set PWM frequency  1000*(1/8e6) =  12.5e-5 seconds  (4KHz)               
 	OCR1B = 1000;   //set Duty Ratio     1000*(1/8e6) =  12.5e-5 seconds (4KHz)    
-	OCR3A = 100;  //set PWM frequency  100*(1/8e6) =  12.5e-5 seconds               
-	OCR3B = 50;   //set Duty Ratio     100*(1/8e6) =  12.5e-5 seconds              
-	sei();
+	OCR3A = 2000;  //set PWM frequency  100*(1/8e6) =  12.5e-5 seconds               
+	OCR3B = 50;   //set Duty Ratio     100*(1/8e6) =  12.5e-5 seconds   
+/************************Set up PWM timers***************************************/
+
+/************************Set up ADCs***************************************/   
+	//set Reference to internal 2.56 volts
+	//Set ADC data register to be left aligned
+	//Set ADC MUX to read from ADC Channel 3
+	ADMUX = 
+		(1 << REFS1) |
+		(1 << REFS0) |
+		(1 << ADLAR) |
+		(0 << MUX4) |
+		(0 << MUX3) |
+		(0 << MUX2) |
+		(1 << MUX1) |
+		(1 << MUX0);
+		
+	ADCSRA = 
+		(1 << ADEN) | //enable ADC
+		(1 << ADSC) | //Start free-running mode conversions	
+/************************Set up ADCs***************************************/    
+	sei(); //Enable all interrupts
 	
 
 	count = 0;
